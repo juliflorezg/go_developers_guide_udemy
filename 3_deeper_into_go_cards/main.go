6fe327d41d3bb28a5113f33546d46f853182afba
Julian Florez <forezjuli08@hotmail.com>
2023-10-20 11:48:07 -0500

    add - function and return types

--- 3_deeper_into_go_cards/main.go
+++ 3_deeper_into_go_cards/main.go
@@ -4,8 +4,12 @@ import "fmt"
 
 func main() {
 	// var card string = "Ace of spades"
-	card := "Ace of spades"
+	card := newCard()
 	// card = "Five of spades "
 
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of diamonds"
 }
