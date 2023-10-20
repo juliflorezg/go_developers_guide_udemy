25000b9aba23c65db009ffca46ba8e60469bd728
Julian Florez <forezjuli08@hotmail.com>
2023-10-20 16:26:39 -0500

    add - create a new deck

--- 3_deeper_into_go_cards/deck.go
+++ 3_deeper_into_go_cards/deck.go
@@ -5,6 +5,23 @@ import "fmt"
 //create a new type of 'deck'
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+// receiver of the function -> any variable of type 'deck' now gets access to the "print" method
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
--- 3_deeper_into_go_cards/main.go
+++ 3_deeper_into_go_cards/main.go
@@ -3,13 +3,8 @@ package main
 // go run main.go deck.go
 // to run the program with all the package's files
 func main() {
-	cards := deck{"Ace of diamonds", newCard()}
-	cards = append(cards, "Six of spades")
+	cards := newDeck()
 
 	cards.print()
 	// fmt.Println(cards)
-}
-
-func newCard() string {
-	return "Five of diamonds"
 }
