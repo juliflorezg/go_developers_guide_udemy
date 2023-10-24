12561f2fc6f78fca96f5715559c286a9e7e674c2
Julian Florez <forezjuli08@hotmail.com>
2023-10-24 17:24:42 -0500

    add - iterating over a map

--- 5_maps/main.go
+++ 5_maps/main.go
@@ -3,20 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#00ff00",
-	// 	"blue":  "#0000ff",
-	// }
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"blue":  "#0000ff",
+	}
 
 	// var colors map[string]string
 
-	colors := make(map[string]string)
+	// colors := make(map[string]string)
 
 	colors["white"] = "#ffffff"
 
-	fmt.Println(colors)
-	delete(colors, "white")
+	// fmt.Println(colors)
+	// delete(colors, "white")
 
-	fmt.Println(colors)
+	// fmt.Println(colors)
+
+	printMap(colors)
+
+}
+
+func printMap(c map[string]string) {
+	for color, code := range c {
+		fmt.Printf("this is the code for the %v color: %v\n", color, code)
+	}
 }
