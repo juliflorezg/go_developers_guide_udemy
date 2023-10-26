3150e30768c3b1f6943e028d7d17138a721eec39
Julian Florez <forezjuli08@hotmail.com>
2023-10-26 16:35:04 -0500

    add - custom writer

--- 6_interfaces_http/main.go
+++ 6_interfaces_http/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct {
+}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -16,9 +20,22 @@ func main() {
 
 	}
 
-	// byte slice
-	bs := make([]byte, 99999)
-	resp.Body.Read(bs)
-	fmt.Println(string(bs))
+	lw := logWriter{}
 
+	// byte slice
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+	// io.Copy(os.Stdout, resp.Body)
+	io.Copy(lw, resp.Body)
+
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	num, err := fmt.Println(string(bs))
+
+	fmt.Println("Just wrote this many bytes:", num)
+
+	return num, err
 }
